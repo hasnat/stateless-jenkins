e35d7a6dab82cdab5f31c85b10fea76af7b444af
Hasnat Ullah <hasnat.ullah@gmail.com>
2018-04-06 15:06:30 +0100

    Adds jenkins-slave container capable of running docker (via host)

--- init.d/03.examples.groovy
+++ init.d/03.examples.groovy
@@ -8,7 +8,14 @@ import org.jenkinsci.plugins.workflow.job.*
 import org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition
 
 def instance = Jenkins.getInstance()
-"playground example1 example2 example3 example4 example5".split().each {
+"playground \
+example1 \
+example2 \
+example3 \
+example4 \
+example5 \
+example6 \
+example7".split().each {
     println "Set Example Project" + it
     WorkflowJob wp = instance.createProject(WorkflowJob.class, it)
 
---
+++ init.d/05.slave.groovy
@@ -0,0 +1,25 @@
+import hudson.model.*
+import jenkins.model.*
+import hudson.slaves.*
+import hudson.slaves.EnvironmentVariablesNodeProperty.Entry
+
+Slave agent = new DumbSlave(
+        "docker-agent",     // if changing this make sure you edit the secret accordingly
+        "/home/jenkins/agent",
+        new JNLPLauncher())
+agent.nodeDescription = "Node for all your docker needs"
+agent.numExecutors = 4
+agent.labelString = "docker-agent"
+agent.mode = Node.Mode.NORMAL
+agent.retentionStrategy = new RetentionStrategy.Always()
+
+List<Entry> env = new ArrayList<Entry>();
+env.add(new Entry("key1","value1"))
+env.add(new Entry("key2","value2"))
+EnvironmentVariablesNodeProperty envPro = new EnvironmentVariablesNodeProperty(env)
+
+agent.getNodeProperties().add(envPro)
+
+Jenkins.instance.addNode(agent)
+
+println "Node docker-agent has been created successfully."
